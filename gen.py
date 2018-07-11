0000000a1916bdcb76819be4cd40fb18a99be9c7
LastOne817 <lastone817@naver.com>
2018-07-11 20:21:40 +0900

    feat: apply multiprocessing
    aqfpOe

--- gen.py
+++ gen.py
@@ -5,9 +5,10 @@ import re
 from datetime import datetime
 import itertools
 import string
+import multiprocessing as mp
 
 
-def gen(msg):
+def gen(idx, n_procs, msg, q):
     repeat = 6
     l = len(msg) + repeat + 2
     data = b'commit %d\0%s' % (l, msg)
@@ -18,19 +19,29 @@ def gen(msg):
     for t in range(now + 120, now + 86400):
         new_str = r.search(data)[1] + str(t).encode() + r.search(data)[3]
         replaced = r.sub(new_str, data)
-        for nonce in itertools.product(string.ascii_letters, repeat=repeat):
+        for nonce in itertools.islice(itertools.product(string.ascii_letters, repeat=repeat), idx, len(string.ascii_letters) ** repeat, n_procs):
             nonce = ''.join(nonce)
             padded = replaced + b'\n' + nonce.encode() + b'\n'
             commit_hash = hashlib.sha1(padded).hexdigest()
             if commit_hash[:difficulty] == '0' * difficulty:
-                return t, nonce
+                q.put((t, nonce))
 
 
 if __name__ == '__main__':
+    n_procs = 32
+    ctx = mp.get_context('spawn')
+    q = ctx.Queue()
+    plist = list()
     sp = subprocess.run(['git', 'cat-file', 'commit', 'HEAD'], stdout=subprocess.PIPE)
     msg = sp.stdout
+    for idx in range(n_procs):
+        p = ctx.Process(target=gen, args=(idx, n_procs, msg, q))
+        p.start()
+        plist.append(p)
 
-    t, nonce = gen(msg)
+    t, nonce = q.get()
+    for p in plist:
+        p.terminate()
 
     print(t)
     print(datetime.fromtimestamp(int(t)).strftime('%Y-%m-%d %H:%M:%S'))
